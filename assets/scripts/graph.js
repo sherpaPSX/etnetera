397d7c502ccc16f33b800d8984d9fe02272a7f60
Lukáš Werner <sherpa.psx@gmail.com>
2016-07-20 13:13:01 +0200

    graph fix

--- assets/scripts/graph.js
+++ assets/scripts/graph.js
@@ -6,52 +6,35 @@ $(document).ready(function() {
 
 function graph(){
 	if ($(window).width() < 380) {
-    	$('.graphs .graph .item').each(function(){
-			var percent = $(this).data('percent');
-			var color = '#e84e0f';
-
-
-			if(percent < 33) {
-				color = '#ffc59b';
-			} else if (percent < 66){
-				color = '#f88c6c';
-			} 
-
-			$(this).width(percent).height(20).css({'background': color, 'margin-top': 0});
-			$(this).find('span').html(percent + '%');
-
-		});
+    	loop(1);
   	} else if ($(window).width() < 650) {
-    	$('.graphs .graph .item').each(function(){
-			var percent = $(this).data('percent');
-			var color = '#e84e0f';
-
-
-			if(percent < 33) {
-				color = '#ffc59b';
-			} else if (percent < 66){
-				color = '#f88c6c';
-			} 
-
-			$(this).width(percent*2).height(20).css({'background': color, 'margin-top': 0});
-			$(this).find('span').html(percent + '%');
-
-		});
+    	loop(2);
  	} else {
- 		$('.graphs .graph .item').each(function(){
-			var percent = $(this).data('percent');
-			var color = '#e84e0f';
-
-
-			if(percent < 33) {
-				color = '#ffc59b';
-			} else if (percent < 66){
-				color = '#f88c6c';
-			} 
-
-			$(this).height(percent).width(20).css({'background': color, 'margin-top': 100-percent});
-			$(this).find('span').html(percent + '%');
-
-		});
+ 		loop(3);
  	}
+}
+
+function loop(type) {
+	$('.graphs .graph .item').each(function(){
+		var percent = $(this).data('percent');
+		var color = '#e84e0f';
+
+
+		if(percent < 33) {
+			color = '#ffc59b';
+		} else if (percent < 66){
+			color = '#f88c6c';
+		} 
+
+		if(type == 1) {
+			$(this).width(percent).height(20).css({'background': color, 'margin-top': 0});
+		} else if (type == 2) {
+			$(this).width(percent*2).height(20).css({'background': color, 'margin-top': 0});
+		} else if (type == 3) {
+			$(this).height(percent).width(20).css({'background': color, 'margin-top': 100-percent});
+		}
+			
+		$(this).find('span').html(percent + '%');
+
+	});
 }
